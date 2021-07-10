17b35d420f3246636b6595afd1deba0491b88e40
moekidev <me@moeki.dev>
2021-07-11 07:10:25 +0900

    Fix

--- src/components/ArtistInput.vue
+++ src/components/ArtistInput.vue
@@ -3,7 +3,7 @@
           v-model="input"
           @change="enter(input)" 
           placeholder="Your favorite artist"
-          class="text-4xl p-5 w-full my-5 box-border
+          class="text-4xl p-4 w-full my-3 box-border
                   border-b-2 bg-black border-gray-800 text-white
                   outline-none"
           autofocus />
