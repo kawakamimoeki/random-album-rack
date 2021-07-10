c9cc8b23005072dce700bd8a9a9e756261f8bbd5
moekidev <me@moeki.dev>
2021-07-11 07:06:41 +0900

    Fix

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-col
               xl:w-1/5 lg:w-1/4 md:w-1/3 w-1/2
-              box-border p-4">
+              box-border p-2">
     <img :src="album.images[1].url"
         class="w-full rounded-2xl" />
-    <div class="p-4">
+    <div class="p-2">
       <h5 class="text-lg">
         <a :href="googleSearchLink(album)"
             target="_blank" rel="noopener noreferrer"
@@ -12,7 +12,7 @@
           {{ album.name }}
         </a>
       </h5>
-      <h6 class="pt-2 text-sm opacity-50 underline cursor-pointer"
+      <h6 class="text-sm opacity-50 underline cursor-pointer"
           @click="selectArtist(album)">
         {{ album.artists[0].name }}
       </h6>
